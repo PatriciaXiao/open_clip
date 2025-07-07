e0ebb10d99e91a25d798d06f3f331e333d0c1e37
patricia_xiao <patricia.xiao@gmail.com>
2025-07-07 00:18:19 -0500

    updates

---
+++ scripts/my_sample.sh
@@ -0,0 +1,15 @@
+# run on cuda:1, if use 0 and 2 then CUDA_VISIBLE_DEVICES=0,2 
+CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node 2 python -m open_clip_train.main \
+  --train-data "../../mydataset/my_sample.tar" \
+  --dataset-type webdataset \
+  --warmup 0 \
+  --batch-size 2 \
+  --lr 1e-5 \
+  --wd 0.0 \
+  --epochs 1 \
+  --workers 0 \
+  --model ViT-B-32 \
+  --pretrained openai \
+  --logs ./logs_test \
+  --output-dir ./checkpoints_test \
+  --report-to none
