ca94989f5c1206cf5c4a594ffbeb120006fa5536
patricia_xiao <patricia.xiao@gmail.com>
2025-07-07 01:42:51 -0500

    Update example_usage_local.py

--- example_usage_local.py
+++ example_usage_local.py
@@ -2,18 +2,20 @@ import torch
 from open_clip import create_model_and_transforms, tokenize
 from PIL import Image
 
+device = "cuda:1" if torch.cuda.is_available() else "cpu"
+
 # Load model
 model_path = "src/logs_test/2025_07_06-23_16_02-model_ViT-B-32-lr_1e-05-b_2-j_0-p_amp/checkpoints/epoch_1.pt"
 model, _, preprocess = create_model_and_transforms(
-    'ViT-B-32', pretrained=model_path, device='cuda:1'  # or 'cpu'
+    'ViT-B-32', pretrained=model_path, device=device
 )
 model.eval()
 
 # Prepare example input
-image = preprocess(Image.open("mydataset/sample_data/images/0003.png")).unsqueeze(0).half()
-text = tokenize(["a diagram", "a dog", "a cat"])
+image = preprocess(Image.open("mydataset/sample_data/images/0003.png")).unsqueeze(0).to(device).half()
+text = tokenize(["a diagram", "a dog", "a cat"]).to(device)
 
-with torch.no_grad(), torch.autocast("cuda:1"):
+with torch.no_grad(), torch.autocast(device):
     image_features = model.encode_image(image)
     text_features = model.encode_text(text)
     image_features /= image_features.norm(dim=-1, keepdim=True)
