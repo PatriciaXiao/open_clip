fa75483dd2a86146c336fc2f3e272673455fe841
patricia_xiao <patricia.xiao@gmail.com>
2025-07-07 02:40:56 -0500

    Update check_version.py

--- check_version.py
+++ check_version.py
@@ -4,7 +4,7 @@ def identify_vit_arch(checkpoint_path):
     print(f"📦 Loading checkpoint: {checkpoint_path}")
     
     ckpt = torch.load(checkpoint_path, map_location="cpu")
-    state_dict = ckpt.get("model", ckpt)["teacher"]
+    state_dict = ckpt.get("model", ckpt)#["teacher"]# needed for orig ver
 
     #print(state_dict.keys())
     print("Checkpoint keys:")
