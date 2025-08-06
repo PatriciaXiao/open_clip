c146288983d35cb71837fe85309a9e5d8c89d3a8
patricia_xiao <patricia.xiao@gmail.com>
2025-08-05 21:31:34 -0500

    Update my_train.sh

--- scripts/my_train.sh
+++ scripts/my_train.sh
@@ -1,4 +1,5 @@
 # run on cuda:1, if use 0 and 2 then CUDA_VISIBLE_DEVICES=0,2 
+# to process more samples from /homes/gws/patxiao/open_clip/mydataset/mydata.jsonl
 CUDA_VISIBLE_DEVICES=1 python -m open_clip_train.main \
   --train-data "../mydataset/sample_data/my_sample.tar" \
   --pretrained "/projects/chimera/nobackup/wliu25/ckpts/pretrain/ckpt_lshort_bs128/eval/training_99999/teacher_checkpoint.pth" \
