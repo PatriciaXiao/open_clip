f9b8f5048f872d56f5f61945ec8f0ae7106655cd
patricia_xiao <patricia.xiao@gmail.com>
2025-08-05 20:39:19 -0500

    Update data.py

--- code_ref/hzhang_code/src/open_clip_train/data.py
+++ code_ref/hzhang_code/src/open_clip_train/data.py
@@ -319,7 +319,7 @@ def group_by_keys_nothrow(data, keys=base_plus_ext, lcase=True, suffixes=None, h
     current_sample = None
     for filesample in data:
         assert isinstance(filesample, dict)
-        #print(filesample.keys()) # dict_keys(['fname', 'data', '__url__'])
+        print(filesample.keys()) # dict_keys(['fname', 'data', '__url__'])
         #exit(0)
         fname, value = filesample["fname"], filesample["data"]
         prefix, suffix = keys(fname)
